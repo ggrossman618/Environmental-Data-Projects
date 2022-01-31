163c64ff50c91fa86e4bb369d7f3e412981670cc
ggrossman618 <ggrossman@colgate.edu>
2022-01-31 12:11:19 -0500

    in-class activity 2 work

--- activity2InClassWork.R
+++ activity2InClassWork.R
@@ -3,3 +3,15 @@
 
 # Make a vector
 heights <- c(30,41,20,22)
+
+# example vector multiplication
+heights_cm <- heights*100
+
+# create a vector from 1-99
+larger_vector <- 1:99
+
+# reverse large vector
+reverse_vector <- 99:1
+
+# create a matrix
+my_matrix <- matrix(c(1,2,3,4,5,6), ncol=2, byrow=TRUE)
