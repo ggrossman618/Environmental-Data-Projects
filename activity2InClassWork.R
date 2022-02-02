0150d05360bcccd685c076ebd7a17476a27e40b3
ggrossman618 <ggrossman@colgate.edu>
2022-02-02 11:47:43 -0500

    read file data into variable

--- activity2InClassWork.R
+++ activity2InClassWork.R
@@ -1,6 +1,9 @@
 # In-class work for activity 2
 # 01/21/22
 
+# Set working directory to my noaa data folder 
+setwd("Z:/students/ggrossman/data/noaa_weather")
+
 # Make a vector
 heights <- c(30,41,20,22)
 
@@ -15,3 +18,6 @@ reverse_vector <- 99:1
 
 # create a matrix
 my_matrix <- matrix(c(1,2,3,4,5,6), ncol=2, byrow=TRUE)
+
+# read weather data into variable
+dataW <- read.csv("2011124.csv")
