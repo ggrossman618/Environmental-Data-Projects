044a91187f109543b32a8cde42e9e73d96ad3e96
ggrossman618 <ggrossman@colgate.edu>
2022-02-04 11:12:59 -0500

    completed first vector section of activity 2

--- activity2InClassWork.R
+++ activity2InClassWork.R
@@ -9,6 +9,9 @@ heights <- c(30,41,20,22)
 
 # example vector multiplication
 heights_cm <- heights*100
+heights_cm
+heights[1] #heights vector at element 1, should return 30
+heights[2:3]#heights vector between (inclusive) element 2 & 3, should return 41, 20
 
 # create a vector from 1-99
 larger_vector <- 1:99
