79ac425a1849f9ea3d90daf2fd54d9c93802ede6
ggrossman618 <ggrossman@colgate.edu>
2022-04-13 12:00:52 -0400

    Work from 4/13/22

--- Apr6InClassWork.R
+++ Apr6InClassWork.R
@@ -1,19 +1,41 @@
-#load the terra package
+# load the terra package
 library(terra)
 
-#set working directory
+# set working directory
 setwd("Z:/students/ggrossman/")
 
-#read a raster from file
+# read a raster from file
 p <- rast("Z:/students/ggrossman/data/rs_data/20190706_002918_101b_3B_AnalyticMS_SR.tif")
 
-#plot the raster
+# plot the raster
 plot(p)
 
-#plot an rgb rendering of the data
+# plot an rgb rendering of the data
 plotRGB(p, r=3, g=2, b=1)
 
-#plot an rgb rendering of the data
+# plot an rgb rendering of the data
 plotRGB(p, r=3, g=2, b=1,
         scale=65535,
         stretch="hist")
+
+# read data file with field observations of canopy cover
+tree <- read.csv("Z:/data/rs_data/siberia_stand_data.csv", header = T)
+
+# convert to vector object using terra package
+gtree <- vect(tree, geom=c("Long", "Lat"), "epsg:4326")
+
+# project the data to match the coordinate system of the raster layer
+# ext() min and max long and lat
+gtree2 <- project(gtree, p)
+
+# create a polygon from the extent of the points
+b <- as.lines(ext(gtree), "epsg:4326")
+
+# reproject the polygons to the same projection as our raster
+b2 <- project(b, crs(p))
+
+# buffer the extent by 200m
+b3 <- buffer(b2, width = 200)
+
+# use this to crop the raster layer so we
+p2 <- crop(p, b3, filename = "20")
