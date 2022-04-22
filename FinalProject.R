697e331fcaa06cebd6a2e5c86965bf1c21ab33a0
ggrossman618 <ggrossman@colgate.edu>
2022-04-22 12:11:17 -0400

    Working graph for co2 nsae

--- FinalProject.R
+++ FinalProject.R
@@ -2,17 +2,42 @@
 #install.packages('R.utils')
 #install.packages("BiocManager")
 BiocManager::install("rhdf5")
-install.packages("BiocManager")
+install.packages("neonUtilities")
 
 # Libraries
 library(data.table)
 library("rhdf5")
+library(neonUtilities)
 
 # set working directory
 setwd("Z:/students/ggrossman/")
 
+
+f <- "Z:/GEOG331_S22/students/ggrossman/data/NEON_eddy-flux/NEON.D03.DSNY.DP4.00200.001.2017-03.basic.20220120T173946Z.RELEASE-2022/NEON.D03.DSNY.DP4.00200.001.nsae.2017-03.basic.20211220T152138Z.h5/NEON.D03.DSNY.DP4.00200.001.nsae.2017-03.basic.20211220T152138Z.h5"
+
 # File URL
 fileUrl <- "Z:/students/ggrossman/data/NEON_eddy-flux/NEON.D03.DSNY.DP4.00200.001.2017-03.basic.20220120T173946Z.RELEASE-2022/NEON.D03.DSNY.DP4.00200.001.nsae.2017-03.basic.20211220T152138Z.h5/NEON.D03.DSNY.DP4.00200.001.nsae.2017-03.basic.20211220T152138Z.h5"
+
+fileUrl2 <- "Z:/students/ggrossman/data/NEON_eddy-flux/NEON.D03.DSNY.DP4.00200.001.2020-07.basic.20220120T173946Z.RELEASE-2022/NEON.D03.DSNY.DP4.00200.001.nsae.2020-07.basic.20211220T175101Z.h5"
+  
+
+
+# Read in data to flux variable
+flux <- stackEddy(filepath = fileUrl,
+                  level = "dp04")
+
+# Read in data 2 to flux variable
+flux2 <- stackEddy(filepath = fileUrl2,
+                  level = "dp04")
+
+# Plot Graph
+plot(flux2$DSNY$data.fluxCo2.nsae.flux ~flux$DSNY$timeBgn, 
+     type="l", pch=".", xlab="Time", ylab="CO2 flux")
+
+
+# list.files will list everything in working directory, recursive = true, simple pattern matching, fullpath = true gives full path
+
+# for loop to unzip all files, list all hdf5 files, write another loop to read NSAE variables and length of it 
 
 # Unzip .gz file
 gunzip(fileUrl)
