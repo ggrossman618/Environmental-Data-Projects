863e23b6a99fea5cec2fbc06afdcabaed97567fe
ggrossman618 <ggrossman@colgate.edu>
2022-04-27 21:40:33 -0400

    Progress in combining vectors to compare trends, hopefully complete sample soon

--- FinalProject.R
+++ FinalProject.R
@@ -1,8 +1,8 @@
 # install -- Only do once!
 #install.packages('R.utils')
 #install.packages("BiocManager")
-BiocManager::install("rhdf5")
-install.packages("neonUtilities")
+#BiocManager::install("rhdf5")
+#install.packages("neonUtilities")
 
 # Libraries
 library(data.table)
@@ -18,27 +18,33 @@ f <- "Z:/GEOG331_S22/students/ggrossman/data/NEON_eddy-flux/NEON.D03.DSNY.DP4.00
 fileUrl <- "Z:/students/ggrossman/data/NEON_eddy-flux/NEON.D03.DSNY.DP4.00200.001.2017-03.basic.20220120T173946Z.RELEASE-2022/NEON.D03.DSNY.DP4.00200.001.nsae.2017-03.basic.20211220T152138Z.h5/NEON.D03.DSNY.DP4.00200.001.nsae.2017-03.basic.20211220T152138Z.h5"
 fileUrl2 <- "Z:/students/ggrossman/data/NEON_eddy-flux/NEON.D03.DSNY.DP4.00200.001.2020-07.basic.20220120T173946Z.RELEASE-2022/NEON.D03.DSNY.DP4.00200.001.nsae.2020-07.basic.20211220T175101Z.h5"
 
-
+# Create overall data frame
+overallFrame <- data.frame()
 
 # Read in data to flux variable
 # MARCH 2017
-flux <- stackEddy(filepath = fileUrl,
+flux1 <- stackEddy(filepath = fileUrl,
                   level = "dp04")
 # JULY 2020
 flux2 <- stackEddy(filepath = fileUrl2,
                   level = "dp04")
 
 # Plot Graph
-plot(flux2$DSNY$data.fluxCo2.nsae.flux ~flux$DSNY$timeBgn, 
+plot(flux2$DSNY$data.fluxCo2.nsae.flux ~ flux2$DSNY$timeBgn, 
      type="l", pch=".", xlab="Time", ylab="CO2 flux")
 
+#create vectors to go into data frame
+allCo2FluxNsae <- flux2$DSNY$data.fluxCo2.nsae.flux
+append(allCo2FluxNsae, flux1$DSNY$data.fluxCo2.nsae.flux)
 
+allTimes <- flux2$DSNY$timeBgn
+append(allTimes, flux1$DSNY$timeBgn)
 
+# plot all co2 nsae fluxes in co2 flux nsae vector with all times in time vector
+plot(allCo2FluxNsae ~ allTime, 
+     type="l", pch=".", xlab="Time", ylab="CO2 flux")
 
-
-
-
-
+# add elements to overall vector
 
 
 
