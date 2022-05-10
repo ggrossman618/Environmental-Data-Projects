b85b946423ac49107f02d199362aecf766578903
ggrossman618 <ggrossman@colgate.edu>
2022-05-10 18:50:03 -0400

    a LOT of progress

--- FinalProject.R
+++ FinalProject.R
@@ -1,23 +1,22 @@
 # install -- Only do once!
 #install.packages('R.utils')
 #install.packages("BiocManager")
-#BiocManager::install("rhdf5")
+#install.packages("DescTools")
 #install.packages("neonUtilities")
+#install.packages("imputeTS")
+#BiocManager::install("rhdf5")
 
 # Libraries
 library(data.table)
 library("rhdf5")
 library(neonUtilities)
 library(stringr)
+library(DescTools)
+library(imputeTS)
+library(devtools)
 
 # set working directory
 setwd("Z:/students/ggrossman/")
-
-
-# saving file paths
-f <- "Z:/GEOG331_S22/students/ggrossman/data/NEON_eddy-flux/NEON.D03.DSNY.DP4.00200.001.2017-03.basic.20220120T173946Z.RELEASE-2022/NEON.D03.DSNY.DP4.00200.001.nsae.2017-03.basic.20211220T152138Z.h5/NEON.D03.DSNY.DP4.00200.001.nsae.2017-03.basic.20211220T152138Z.h5"
-fileUrl <- "Z:/students/ggrossman/data/NEON_eddy-flux/NEON.D05.UNDE.DP4.00200.001.2021-04.basic.20220120T173946Z.RELEASE-2022/NEON.D05.UNDE.DP4.00200.001.nsae.2021-04.basic.20211221T022406Z.h5"
-fileUrl2 <- "Z:/students/ggrossman/data/NEON_eddy-flux/NEON.D03.DSNY.DP4.00200.001.2020-07.basic.20220120T173946Z.RELEASE-2022/NEON.D03.DSNY.DP4.00200.001.nsae.2020-07.basic.20211220T175101Z.h5"
 
 # unzip files
 filepaths <- list.files(path = "data/NEON_eddy-flux/", pattern = ".gz", full.names = T, recursive = T)
@@ -32,13 +31,7 @@ while(i < length(outfile)+1){
   i <- i+1
 }
 
-
-
-
-# load all data into variable
-eddy_data <- stackEddy(filepath = "data/test_eddy/",
-                  level = "dp04")
-
+# load all data into separate variable
 DSNY_eddy_data <- stackEddy(filepath = "data/DSNY/",
                          level = "dp04")
 
@@ -51,70 +44,236 @@ STER_eddy_data <- stackEddy(filepath = "data/STER/",
 ABBY_eddy_data <- stackEddy(filepath = "data/ABBY/",
                             level = "dp04")
 
-
-
-# plot DSNY nsae eddy data
-# plot DSNY co2 nsae fluxes 
+# plot eddy nsae graphs for all locations
 plot(DSNY_eddy_data$DSNY$data.fluxCo2.nsae.flux ~ DSNY_eddy_data$DSNY$timeBgn, 
-     type="l", pch=".", xlab="Time", ylab="CO2 flux")
+     type="l", pch=".", xlab="Time", ylab="CO2 flux", #xlim = c(UNDE_eddy_data$UNDE$timeBgn[20000], UNDE_eddy_data$UNDE$timeBgn[40000]),
+     ylim = c(-100, 100))
 
 # plot UNDE co2 nsae fluxes
 plot(UNDE_eddy_data$UNDE$data.fluxCo2.nsae.flux ~ UNDE_eddy_data$UNDE$timeBgn, 
-     type="l", pch=".", xlab="Time", ylab="CO2 flux", # xlim = c(UNDE_eddy_data$UNDE$timeBgn[20000], UNDE_eddy_data$UNDE$timeBgn[40000]),
+     type="l", pch=".", xlab="Time", ylab="CO2 flux", #xlim = c(UNDE_eddy_data$UNDE$timeBgn[20000], UNDE_eddy_data$UNDE$timeBgn[40000]),
      ylim = c(-100, 100))
 
+# plot STER co2 nsae flux
+plot(STER_eddy_data$STER$data.fluxCo2.nsae.flux ~ STER_eddy_data$STER$timeBgn, 
+     type="l", pch=".", xlab="Time", ylab="CO2 flux", #xlim = c(UNDE_eddy_data$UNDE$timeBgn[20000], UNDE_eddy_data$UNDE$timeBgn[40000]),
+     ylim = c(-100, 100))
 
+# plot ABBY co2 nsae flux
+plot(ABBY_eddy_data$ABBY$data.fluxCo2.nsae.flux ~ ABBY_eddy_data$ABBY$timeBgn, 
+     type="l", pch=".", xlab="Time", ylab="CO2 flux", #xlim = c(UNDE_eddy_data$UNDE$timeBgn[20000], UNDE_eddy_data$UNDE$timeBgn[40000]),
+     ylim = c(-100, 100))
 
+# save co2 nsae data into new variables to avoid overwriting them
+DSNY_NSAE_limited <- DSNY_eddy_data$DSNY$data.fluxCo2.nsae.flux
+UNDE_NSAE_limited <- UNDE_eddy_data$UNDE$data.fluxCo2.nsae.flux
+STER_NSAE_limited <- STER_eddy_data$STER$data.fluxCo2.nsae.flux
+ABBY_NSAE_limited <- ABBY_eddy_data$ABBY$data.fluxCo2.nsae.flux
 
-fluxFrame <- data.frame(fluxDSNY = DSNY_eddy_data$DSNY$data.fluxCo2.nsae.flux)
-fluxFrame$fluxUNDE <- UNDE_eddy_data$UNDE$data.fluxCo2.nsae.flux
-
-# populate data frame with dp04 files NOT WORKING YET
-i <- 2
-while(i < length(outfile)+1){
-    tempFlux <- stackEddy(filepath = unzippedFilePaths[i],
-                          level = "dp04")
-    fluxFrame$FILENAME = tempFlux
-  i <- i + 1
+# remove all incorrect DSNY readings by implementing a limit
+for(i in 1:length(DSNY_NSAE_limited)){
+  if(is.na(DSNY_NSAE_limited[i]) == FALSE){
+    if(DSNY_NSAE_limited[i] > 50 | DSNY_NSAE_limited[i] < -50){
+      DSNY_NSAE_limited[i] <- NA
+    }
+  }
 }
 
+#interpolate the DSNY NA values
+DSNY_interpolations <- na_interpolation(DSNY_NSAE_limited, option = "spline")
 
+#add all interpolated values into limited co2 nsae data
+iterator <- 1
+for(i in 1:length(DSNY_NSAE_limited)){
+  if(is.na(DSNY_NSAE_limited[i]) == TRUE){
+    DSNY_NSAE_limited[i] <- DSNY_interpolations[iterator]
+  }
+}
 
+# remove all incorrect UNDE readings by implementing a limit
+for(i in 1:length(UNDE_NSAE_limited)){
+  if(is.na(UNDE_NSAE_limited[i]) == FALSE){
+    if(UNDE_NSAE_limited[i] > 50 | UNDE_NSAE_limited[i] < -50){
+      UNDE_NSAE_limited[i] <- NA
+    }
+  }
+}
 
+#interpolate the UNDE NA values
+UNDE_interpolations <- na_interpolation(UNDE_NSAE_limited, option = "spline")
 
+#add all interpolated values into limited co2 nsae data
+iterator <- 1
+for(i in 1:length(UNDE_NSAE_limited)){
+  if(is.na(UNDE_NSAE_limited[i]) == TRUE){
+    UNDE_NSAE_limited[i] <- UNDE_interpolations[iterator]
+  }
+}
 
+# remove all incorrect STER readings by implementing a limit
+for(i in 1:length(STER_NSAE_limited)){
+  if(is.na(STER_NSAE_limited[i]) == FALSE){
+    if(STER_NSAE_limited[i] > 50 | STER_NSAE_limited[i] < -50){
+      STER_NSAE_limited[i] <- NA
+    }
+  }
+}
 
+#interpolate the STER NA values
+STER_interpolations <- na_interpolation(STER_NSAE_limited, option = "spline")
 
+#add all interpolated values into limited co2 nsae data
+iterator <- 1
+for(i in 1:length(STER_NSAE_limited)){
+  if(is.na(STER_NSAE_limited[i]) == TRUE){
+    STER_NSAE_limited[i] <- STER_interpolations[iterator]
+  }
+}
 
+# ABBY
+for(i in 1:length(ABBY_NSAE_limited)){
+  if(is.na(ABBY_NSAE_limited[i]) == FALSE){
+    if(ABBY_NSAE_limited[i] > 50 | ABBY_NSAE_limited[i] < -50){
+      ABBY_NSAE_limited[i] <- NA
+    }
+  }
+}
 
+#interpolate the UNDE NA values
+ABBY_interpolations <- na_interpolation(ABBY_NSAE_limited, option = "spline")
 
+#add all interpolated values into limited co2 nsae data
+iterator <- 1
+for(i in 1:length(ABBY_NSAE_limited)){
+  if(is.na(ABBY_NSAE_limited[i]) == TRUE){
+    ABBY_NSAE_limited[i] <- ABBY_interpolations[iterator]
+  }
+}
 
-
-
-
-
-
-
-
-
-
-
-
-# Read in data to flux variable
-# MARCH 2017
-flux1 <- stackEddy(filepath = fileUrl,
-                  level = "dp04")
-# JULY 2020
-flux2 <- stackEddy(filepath = fileUrl2,
-                  level = "dp04")
-
-# Plot Graph
-plot(flux2$DSNY$data.fluxCo2.nsae.flux ~ flux2$DSNY$timeBgn, 
+# plot entire timeline of graphs
+plot(DSNY_NSAE_limited ~ DSNY_eddy_data$DSNY$timeBgn, 
      type="l", pch=".", xlab="Time", ylab="CO2 flux")
 
-#create vectors to go into data frame
-allCo2FluxNsae <- flux2$DSNY$data.fluxCo2.nsae.flux
-append(allCo2FluxNsae, flux1$DSNY$data.fluxCo2.nsae.flux)
+plot(UNDE_NSAE_limited ~ UNDE_eddy_data$UNDE$timeBgn, 
+     type="l", pch=".", xlab="Time", ylab="CO2 flux")
 
-allTimes <- flux2$DSNY$timeBgn
-append(allTimes, flux1$DSNY$timeBgn)
+plot(STER_NSAE_limited ~ STER_eddy_data$STER$timeBgn, 
+     type="l", pch=".", xlab="Time", ylab="CO2 flux")
+
+plot(ABBY_NSAE_limited ~ ABBY_eddy_data$ABBY$timeBgn, 
+     type="l", pch=".", xlab="Time", ylab="CO2 flux")
+
+
+# for loop to create DSNY seasonal graphs
+par(mfrow = c(2,2))
+DSNY_seasonal_plot_indexes <- c(921,5377,9793,14161,18481,22897,27313,31681,36001,40417,44833,
+                                49201,53569,57985,62401,66769,71089,75505,79921,84289, 87648)
+seasons <- c("Spring", "Summer", "Fall", "Winter")
+season <- 1
+year <- 2017
+for(i in 1:21) {
+  plot(DSNY_NSAE_limited ~ DSNY_eddy_data$DSNY$timeBgn, 
+       type="l", pch=".", xlab="Time", ylab="CO2 flux",
+       xlim = c(DSNY_eddy_data$DSNY$timeBgn[DSNY_seasonal_plot_indexes[i]], 
+                DSNY_eddy_data$DSNY$timeBgn[DSNY_seasonal_plot_indexes[i+1]]),
+       main=seasons[season])
+  mtext(paste("DSNY CO2 nsae flux", year, collapse = NULL), side = 3, line = -16, outer = TRUE)
+  if(season != 4){
+    season <- season + 1
+  } else {
+    season <- 1
+    year <- year + 1
+  }
+}
+
+#graph generation below
+
+# for loop to create UNDE seasonal graphs
+par(mfrow = c(2,2))
+UNDE_seasonal_plot_indexes <- c(2305,6721,11137,15505,19825,24241,28657,33025,37345,41761,46117,
+                                50545,54913,59329,63745,68113,72433,76849,81265,85633,88922)
+seasons <- c("Spring", "Summer", "Fall", "Winter")
+season <- 1
+year <- 2017
+for(i in 1:21) {
+  plot(UNDE_NSAE_limited ~ UNDE_eddy_data$UNDE$timeBgn, 
+       type="l", pch=".", xlab="Time", ylab="CO2 flux",
+       xlim = c(UNDE_eddy_data$UNDE$timeBgn[UNDE_seasonal_plot_indexes[i]], 
+                UNDE_eddy_data$UNDE$timeBgn[UNDE_seasonal_plot_indexes[i+1]]),
+       main=seasons[season])
+  mtext(paste("UNDE CO2 nsae flux", year, collapse = NULL), side = 3, line = -16, outer = TRUE)
+  if(season != 4){
+    season <- season + 1
+  } else {
+    season <- 1
+    year <- year + 1
+  }
+}
+
+# for loop to create UNDE seasonal graphs
+par(mfrow = c(2,2))
+UNDE_seasonal_plot_indexes <- c(2305,6721,11137,15505,19825,24241,28657,33025,37345,41761,46117,
+                                50545,54913,59329,63745,68113,72433,76849,81265,85633,88922)
+seasons <- c("Spring", "Summer", "Fall", "Winter")
+season <- 1
+year <- 2017
+for(i in 1:21) {
+  plot(UNDE_NSAE_limited ~ UNDE_eddy_data$UNDE$timeBgn, 
+       type="l", pch=".", xlab="Time", ylab="CO2 flux",
+       xlim = c(UNDE_eddy_data$UNDE$timeBgn[UNDE_seasonal_plot_indexes[i]], 
+                UNDE_eddy_data$UNDE$timeBgn[UNDE_seasonal_plot_indexes[i+1]]),
+       main=seasons[season])
+  mtext(paste("UNDE CO2 nsae flux", year, collapse = NULL), side = 3, line = -16, outer = TRUE)
+  if(season != 4){
+    season <- season + 1
+  } else {
+    season <- 1
+    year <- year + 1
+  }
+}
+
+# for loop to create STER seasonal graphs
+par(mfrow = c(2,2))
+STER_seasonal_plot_indexes <- c(NA,1,3937,8305,12625,17041,21457,25825,30145,34561,38977,43345,47713,
+                                52129,56545,60913,65233,69649,74065,78433,81792)
+seasons <- c("Spring", "Summer", "Fall", "Winter")
+season <- 1
+year <- 2017
+for(i in 1:21) {
+  if(i == 1){
+    plot()
+  }
+  plot(STER_NSAE_limited ~ STER_eddy_data$STER$timeBgn, 
+       type="l", pch=".", xlab="Time", ylab="CO2 flux",
+       xlim = c(STER_eddy_data$STER$timeBgn[STER_seasonal_plot_indexes[i]], 
+                STER_eddy_data$STER$timeBgn[STER_seasonal_plot_indexes[i+1]]),
+       main=seasons[season])
+  mtext(paste("STER CO2 nsae flux", year, collapse = NULL), side = 3, line = -16, outer = TRUE)
+  if(season != 4){
+    season <- season + 1
+  } else {
+    season <- 1
+    year <- year + 1
+  }
+}
+
+fluxFrame <- data.frame(fluxTime = STER_eddy_data$STER$timeBgn)
+
+
+
+
+
+
+
+
+# individual seasons graphs
+plot(DSNY_NSAE_limited ~ DSNY_eddy_data$DSNY$timeBgn, 
+     type="l", pch=".", xlab="Time", ylab="CO2 flux", 
+     xlim = c(DSNY_eddy_data$DSNY$timeBgn[53569], DSNY_eddy_data$DSNY$timeBgn[57985]))
+
+# calculate area under a DSNY plot
+DSNY_AUC <- AUC(DSNY_NSAE_limited, DSNY_eddy_data$DSNY$timeBgn, na.rm = T)
+
+
+
+
